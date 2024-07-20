4cec35c06e66e7f40870ccee8b5eefcf71242c23
markandre0425 <markandre0425@gmail.com>
2024-07-20 20:47:03 +0800

    upload demojav and demophp

---
+++ Demofiles/demojav.java
@@ -0,0 +1,13 @@
+package Demofiles;
+import java.util.Scanner;
+
+/**
+ * demojav
+ */
+public class demojav {
+Scanner name = new Scanner(System.in);
+System.out.println("Enter Name: ");
+String fullName = name.nextLine();
+System.out.println("Welcome: " + fullName);
+    
+}
